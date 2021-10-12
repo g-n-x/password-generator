29b1c55005236dbe8d1ecf4e1f8b159ef6a4e629
g-n-x <gnx@tuta.io>
2021-10-12 10:54:51 -0300

    feat: add character types selection

--- src/components/PassGen.tsx
+++ src/components/PassGen.tsx
@@ -2,11 +2,19 @@ import React, {useState} from 'react'
 import {
     Slider,
     TextField,
-    Typography
+    Typography,
+    ToggleButtonGroup,
+    ToggleButton
   } from '@material-ui/core'
 
 const PassGen: React.FC = () => {
     const [passLen, setPassLen] = useState(8)
+    const [config, setConfig] = useState(['lowercase'])
+
+    const handleConfig = (_event: any, newConfig: any) => {
+        setConfig(newConfig)
+    }
+
     return(
         <div
             style={{
@@ -25,6 +33,20 @@ const PassGen: React.FC = () => {
                 }
             />
             <Typography>Password Length: {passLen}</Typography>
+            <Typography>Character Types</Typography>
+            <ToggleButtonGroup
+                value={config}
+                onChange={handleConfig}
+            >
+                <ToggleButton value="uppercase">A-Z</ToggleButton>
+                <ToggleButton value="lowercase">a-z</ToggleButton>
+                <ToggleButton value="numbers">0-9</ToggleButton>
+                <ToggleButton value="special1"># $ % &amp; @ ^ ` ~</ToggleButton>
+                <ToggleButton value="special2">. , : ;</ToggleButton>
+                <ToggleButton value="special3">" '</ToggleButton>
+                <ToggleButton value="special4">\ / | _ -</ToggleButton>
+                <ToggleButton value="special5">&lt; * + ! ? = &gt;</ToggleButton>
+            </ToggleButtonGroup>
         </div>
     )
 }
