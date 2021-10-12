194fdcea001b19d7387af6a0a009488dd03d1ade
g-n-x <gnx@tuta.io>
2021-10-12 10:31:12 -0300

    feat: add password length slider

--- src/components/PassGen.tsx
+++ src/components/PassGen.tsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {
+    Slider,
+    Typography
+  } from '@material-ui/core'
 
 const PassGen: React.FC = () => {
+    const [passLen, setPassLen] = useState(8)
     return(
-        <><h1>test</h1></>
+        <>
+            <Slider
+                defaultValue={8}
+                style={{width: 396}}
+                max={256}
+                onChange={
+                    (ev, val) => {
+                        setPassLen(Number(val))
+                    }
+                }
+            />
+            <Typography>Password Length: {passLen}</Typography>
+        </>
     )
 }
 
