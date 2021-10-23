6e06da1750e9185c5d3b5731a1fd645d9f40811c
g-n-x <gnx@tuta.io>
2021-10-23 18:55:09 -0300

    add charsets and updatePass function

--- src/components/PassGen.tsx
+++ src/components/PassGen.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState, useCallback} from 'react'
 import {
     Slider,
     TextField,
@@ -8,12 +8,84 @@ import {
     Input
   } from '@material-ui/core'
 
+// char sets
+const lower: string[] = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+]
+
+const upper: string[] = [
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+    'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+]
+
+const numbers: string[] = [
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
+]
+
+const special1: string[] = [
+    '#', '$', '%', '&', '@', '^', '`', '~'
+]
+
+const special2: string[] = [
+    '.', ',', ':', ';'
+]
+
+const special3: string[] = [
+    '"', '\''
+]
+
+const special4: string[] = [
+    '\\', '/', '|', '_', '-'
+]
+
+const special5: string[] = [
+    '<', '*', '+', '!', '?', '=', '>'
+]
+
 const PassGen: React.FC = () => {
     // states
+    const [txtPass, setTxtPass] = useState('')
     const [passLen, setPassLen] = useState(8)
     const [config, setConfig] = useState(['lowercase'])
 
     // functions
+    const updatePass = useCallback(() => {
+        let newPass: string = ''
+        for(let i = 0; i < passLen; ++i) {
+            // between 0 and 6
+            let rand = Math.floor(Math.random() * config.length)
+            let charSet: string = config[rand]
+            switch(charSet) {
+                case 'lowercase':
+                    newPass += lower[Math.floor(Math.random() * lower.length)]
+                    break;
+                case 'uppercase':
+                    newPass += upper[Math.floor(Math.random() * upper.length)]
+                    break;
+                case 'numbers':
+                    newPass += numbers[Math.floor(Math.random() * numbers.length)]
+                    break;
+                case 'special1':
+                    newPass += special1[Math.floor(Math.random() * special1.length)]
+                    break;
+                case 'special2':
+                    newPass += special2[Math.floor(Math.random() * special2.length)]
+                    break;
+                case 'special3':
+                    newPass += special3[Math.floor(Math.random() * special3.length)]
+                    break;
+                case 'special4':
+                    newPass += special4[Math.floor(Math.random() * special4.length)]
+                    break;
+                case 'special5':
+                    newPass += special5[Math.floor(Math.random() * special5.length)]
+                    break;
+            }
+        }
+        setTxtPass(newPass)
+    }, [passLen, config])
+    
     const handleConfig = (_event: any, newConfig: any) => {
         setConfig(newConfig)
     }
@@ -21,10 +93,16 @@ const PassGen: React.FC = () => {
     const handleLenChange = (event: any) => {
         // handles the range of 0 to 256 for event.target.value
         const newVal = Math.min(
-            Math.max(Number(event.target.value), 0), 256
+            Math.max(Number(event.target.value), 0),
+            256
         )
         setPassLen(newVal)
     }
+
+    // effect
+    useEffect(() => {
+        updatePass()
+    }, [updatePass, passLen, config])
 
     return(
         <div
@@ -33,7 +111,10 @@ const PassGen: React.FC = () => {
                 flexDirection: 'column',
             }}
         >
-            <TextField label="password" />
+            <TextField
+                value={txtPass}
+                label="password"
+            />
             <Slider
                 value={passLen}
                 max={256}
@@ -43,9 +124,7 @@ const PassGen: React.FC = () => {
                     }
                 }
             />
-            <div
-                style={{display: 'flex', gap: 20}}
-            >
+            <div style={{display: 'flex', gap: 20}} >
                 <Typography>Password Length: </Typography>
                 <Input
                     type="number"
